1e7165fe44bdd64891bb09b4f3649c49f6c3eaea
NANDY1063 <nandy1063@gmail.com>
2025-03-11 11:27:57 +0530

    updated dash

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,22 @@
 import React from "react"; 
+import dash1  from "../assets/dash1.png";
+import dash2  from "../assets/dash 2.png";
+import dash3  from "../assets/dash3.png";
+
 import "../App.css";
 const Dashboard = () => {
   return (
+    <>
     <nav>
-      <h5>Total Cash Receiving</h5>
+      <h5>Dashboard</h5>
     </nav>
+    <div className="dashimg">
+     <img src={dash3}style={{marginLeft:"2rem", width:"96%", height:"32rem"}}/>
+     <img src={dash1} style={{marginLeft:"2.1rem", width:"47%"}} />
+     <img src={dash2} style={{marginLeft:"2.3rem", width:"47%"}}/> 
+    </div>
+
+     </>
   );
 };
 export default Dashboard;
